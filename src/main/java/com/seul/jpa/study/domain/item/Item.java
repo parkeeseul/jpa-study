64cc5167b3536db16343c829f9b4add18790deeb
parkeeseul <eeseul.p@gmail.com>
2021-04-29 11:25:03 +0900

    feat: Item Entity 비즈니스 로직 추가

--- src/main/java/com/seul/jpa/study/domain/item/Item.java
+++ src/main/java/com/seul/jpa/study/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.seul.jpa.study.domain.item;
 
 import com.seul.jpa.study.domain.Category;
+import com.seul.jpa.study.exception.NotEnoughException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -23,10 +24,29 @@ public abstract class Item {
     private List<Category> categories = new ArrayList<>();
 
     protected Item(String name, int price, int stockQuantity, List<Category> categories) {
-        this.id = id;
         this.name = name;
         this.price = price;
         this.stockQuantity = stockQuantity;
         this.categories = categories;
+    }
+
+    // 비즈니스 로직
+    /**
+     * stock 증가
+     * **/
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * **/
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
     }
 }
